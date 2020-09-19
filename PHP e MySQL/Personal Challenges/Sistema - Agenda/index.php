6ce21588e8500bce9721f4a17239c80e806ff7fc
Luís Galete <luisgalete6264@gmail.com>
2020-09-19 19:19:30 -0300

    Change Usuário

--- PHP e MySQL/Personal Challenges/Sistema - Agenda/index.php
+++ PHP e MySQL/Personal Challenges/Sistema - Agenda/index.php
@@ -9,7 +9,7 @@
   </header>
   <section class="my-4 mx-auto position-relative d-flex flex-column align-items-center justify-content-center rounded" style="width: 60vw; max-width: 700px; height: 60vh; max-height: 500px; padding: 100px; background-color: #f3f5f7;">
     <div id="top-bar" class="bg-info position-absolute fixed-top w-100 rounded d-flex align-items-center" style="height: 50px;">
-      <svg  class="ml-3" width="25px" height="25px" viewBox="0 0 16 16" class="bi bi-person-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style="color: white;">
+      <svg class="ml-3" width="25px" height="25px" viewBox="0 0 16 16" class="bi bi-person-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style="color: white;">
         <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
         <path fill-rule="evenodd" d="M2 15v-1c0-1 1-4 6-4s6 3 6 4v1H2zm6-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
       </svg>
