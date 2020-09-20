c61f0de7fb18ef275a53b9b84d1dc8ec0d0ad83d
Luís Galete <luisgalete6264@gmail.com>
2020-09-20 16:01:39 -0300

    Update usuarios.php layout

--- PHP e MySQL/Personal Challenges/Sistema - Agenda/usuarios.php
+++ PHP e MySQL/Personal Challenges/Sistema - Agenda/usuarios.php
@@ -32,16 +32,16 @@ mysqli_close($conn);
   <nav>
     <?php include 'templates/menu.php'; ?>
   </nav>
-  <main class="mt-4 d-flex align-items-start">
-    <section id="cadastro" class="w-50 mx-3 position-relative d-flex flex-column align-items-center justify-content-center">
-      <div id="top-bar" class="bg-info position-absolute fixed-top rounded d-flex align-items-center w-75 mx-auto" style="height: 50px;">
+  <main class="mt-4 mx-5 d-flex align-items-start justify-content-around">
+    <section id="cadastro" class="w-25 mx-3 position-relative d-flex flex-column align-items-center justify-content-center">
+      <div id="top-bar" class="bg-info position-absolute fixed-top rounded d-flex align-items-center w-100 mx-auto" style="height: 50px;">
         <svg class="ml-3" width="25px" height="25px" viewBox="0 0 16 16" class="bi bi-person-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style="color: white;">
           <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
           <path fill-rule="evenodd" d="M2 15v-1c0-1 1-4 6-4s6 3 6 4v1H2zm6-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
         </svg>
         <h2 class="h4 text-center text-light m-0 ml-2 mb-1" style="font-size: 1.2em;">Cadastrar usuário</h2>
       </div>
-      <form action="config/cadusuario.php" method="POST" class="p-5 w-75 bg-light my-3 d-flex flex-column" style="max-width: 600px; border-radius: 15px;">
+      <form action="config/cadusuario.php" method="POST" class="p-5 w-100 bg-light my-3 d-flex flex-column" style="max-width: 600px; border-radius: 15px;">
         <label for="nome" style="margin-bottom: 0.2em;">Nome: </label>
         <input class="form-control mb-2" type="text" name="nome" id="nome" placeholder="Digite o nome" required>
         <label for="login" style="margin-bottom: 0.2em;">Login: </label>
@@ -54,10 +54,10 @@ mysqli_close($conn);
           <option value="admin">Admin</option>
           <option value="normal">Normal</option>
         </select>
-        <button type="submit" class="btn btn-info w-50 mx-auto mt-4" style=" max-width: 200px;">Cadastrar usuário</button>
+        <button type="submit" class="btn btn-info w-75 mx-auto mt-4" style=" max-width: 200px;">Cadastrar usuário</button>
       </form>
     </section>
-    <section id="usuarios-cadastrados" class="w-50 mx-3 position-relative d-flex flex-column align-items-center justify-content-center">
+    <section id="usuarios-cadastrados" class="w-75 mx-5 position-relative d-flex flex-column align-items-center justify-content-center">
       <div id="top-bar" class="bg-light position-absolute fixed-top rounded d-flex align-items-center justify-content-center w-100 mx-auto border" style="height: 50px;">
         <h2 class="h4 text-center">Usuários cadastrados</h2>
       </div>
@@ -81,7 +81,7 @@ mysqli_close($conn);
               <td><?php echo $usuario['senha']; ?></td>
               <td><?php echo $usuario['tipo']; ?></td>
               <td>
-                <a href="config/editar-usuario.php?id=<?php echo $usuario['id']; ?>" class="btn btn-warning">Editar</a>
+                <a href="config/editar-usuario.php?id=<?php echo $usuario['id']; ?>" class="btn btn-info">Editar</a>
                 <form action="config/excluir-usuario.php" method="POST" class="d-inline">
                   <input type="hidden" name="id-excluir" value="<?php echo $usuario['id']; ?>">
                   <input type="submit" value="Excluir" class="btn btn-danger">
