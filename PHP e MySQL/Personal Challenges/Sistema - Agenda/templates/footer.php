8345626e4eb939a735d20f9ed68d8932f51bb270
Luís Galete <luisgalete6264@gmail.com>
2020-09-21 10:01:49 -0300

    Remove agenda

--- PHP e MySQL/Personal Challenges/Sistema - Agenda/templates/footer.php
+++
@@ -1,5 +0,0 @@
-<hr>
-<footer class="text-center position-relative">
-  <p>Agenda desenvolvida durante aula - SENAI</p>
-  <p>Aperfeiçoado por <a href="https://github.com/yunger7/" target="_blank">Luís Galete</a></p>
-</footer>
