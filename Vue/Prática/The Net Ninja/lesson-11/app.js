8702ef82b4f230a5da34eb824fdfdd1e9a4adcda
Luís Galete <luisgalete6264@gmail.com>
2020-11-19 10:10:35 -0300

    Add lessons 11 and 12

---
+++ Vue/Prática/The Net Ninja/lesson-11/app.js
@@ -0,0 +1,7 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    error: false,
+    success: false
+  }
+});
---
+++ Vue/Prática/The Net Ninja/lesson-12/app.js
@@ -0,0 +1,11 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    videogames: ['PS5', 'Xbox S', 'Nintendo Switch', 'GameBoy'],
+    characters: [
+      {name: 'Ryuunosuke', hobby: 'Programming'},
+      {name: 'Rin', hobby: 'Camping'},
+      {name: 'Holo', hobby: 'Traveling'}
+    ]
+  }
+});
