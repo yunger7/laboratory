345340ea1be545ded58f3fc4e179c6fb297b73e2
Luís Galete <luisgalete6264@gmail.com>
2020-11-23 08:05:47 -0300

    Add lesson 28

---
+++ Vue/Prática/The Net Ninja/dev-env/vuejs/src/components/formTwo.vue
@@ -0,0 +1,18 @@
+<template>
+  <form>
+    <h1>Form two</h1>
+    <input type="email" placeholder="email" required>
+    <textarea cols="30" rows="10" placeholder="Type your text"></textarea>
+    <button>Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
