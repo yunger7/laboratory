16bd16151860099082cfb68b86efe1cc37765574
Luís Galete <luisgalete6264@gmail.com>
2020-11-23 08:38:11 -0300

    Add lesson 30

---
+++ Vue/Prática/The Net Ninja/lesson-30/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <add-blog></add-blog>
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'add-blog': addBlog
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
