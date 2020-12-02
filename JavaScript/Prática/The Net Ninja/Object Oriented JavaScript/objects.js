a2f57347583ab344e7f8e0336fad6bcbf83bf959
Luís Galete <luisgalete6264@gmail.com>
2020-12-02 09:06:00 -0300

    Add lesson 9

--- JavaScript/Prática/The Net Ninja/Object Oriented JavaScript/objects.js
+++ JavaScript/Prática/The Net Ninja/Object Oriented JavaScript/objects.js
@@ -1,39 +1,14 @@
-class User {
-	constructor(email, name) {
-		this.email = email;
-		this.name = name;
-		this.score = 0;
-	}
-	login() {
+function User(email, name) {
+	this.email = email;
+	this.name = name;
+	this.online = false;
+	this.login = function() {
 		console.log(this.email, 'logged in');
-		return this;
-	}
-	logout() {
-		console.log(this.email, 'logged out');
-		return this;
-	}
-	updateScore() {
-		this.score++;
-		console.log(this.email, 'score is now', this.score);
-		return this;
-	}
-}
-
-class Admin extends User {
-	deleteUser(user) {
-		users = users.filter(u => {
-			return u.email != user.email;
-		});
 	}
 }
 
 var userOne = new User('wiseholo@gmail.com', 'Holo');
 var userTwo = new User('cuteshiina@gmail.com', 'Shiina');
-var admin = new Admin('admin@gmail.com', 'Admin');
 
-var users = [userOne, userTwo, admin];
-
-admin.deleteUser(userTwo);
-// userOne.deleteUser(userTwo); doesn't work
-
-console.log(users);
+console.log(userOne);
+userTwo.login();
