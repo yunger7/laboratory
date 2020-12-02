d469248992e030c2f1d79aa97a5e916f0a306f0c
Luís Galete <luisgalete6264@gmail.com>
2020-12-02 11:29:49 -0300

    Add lesson 11

--- JavaScript/Prática/The Net Ninja/Object Oriented JavaScript/objects.js
+++ JavaScript/Prática/The Net Ninja/Object Oriented JavaScript/objects.js
@@ -14,8 +14,24 @@ User.prototype.logout = function() {
 	console.log(this.email, 'logged out');
 }
 
+function Admin(...args) {
+	User.apply(this, args); // This creates a user	
+	// Add admin proprieties
+	this.role = 'super admin';
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function(u) {
+	users = users.filter(user => {
+		return user.email != u.email;
+	})
+};
+
 var userOne = new User('wiseholo@gmail.com', 'Holo');
 var userTwo = new User('cuteshiina@gmail.com', 'Shiina');
+var admin = new Admin('superadmin@gmail.com', 'admin')
 
-console.log(userOne);
-userTwo.login();
+users = [userOne, userTwo, admin];
+
+console.log(users);
