b3049ac7b6a3071a76611cc3cfefd353bf57d0c4
Luís Galete <luisgalete6264@gmail.com>
2020-12-02 08:29:48 -0300

    Add lesson 7

--- JavaScript/Prática/The Net Ninja/Object Oriented JavaScript/objects.js
+++ JavaScript/Prática/The Net Ninja/Object Oriented JavaScript/objects.js
@@ -2,20 +2,24 @@ class User {
 	constructor(email, name) {
 		this.email = email;
 		this.name = name;
+		this.score = 0;
 	}
 	login() {
 		console.log(this.email, 'logged in');
+		return this;
 	}
 	logout() {
 		console.log(this.email, 'logged out');
+		return this;
+	}
+	updateScore() {
+		this.score++;
+		console.log(this.email, 'score is now', this.score);
+		return this;
 	}
 }
 
 var userOne = new User('wiseholo@gmail.com', 'Holo');
 var userTwo = new User('cuteshiina@gmail.com', 'Shiina');
 
-userOne.login();
-userTwo.logout();
-
-console.log(userOne);
-console.log(userTwo);
+userOne.login().updateScore().updateScore().logout();
