52af212f1b2d5043aaab978880cecfbbbe7d83ae
Luís Galete <luisgalete6264@gmail.com>
2020-12-02 09:23:51 -0300

    Add lesson 10

--- JavaScript/Prática/The Net Ninja/Object Oriented JavaScript/objects.js
+++ JavaScript/Prática/The Net Ninja/Object Oriented JavaScript/objects.js
@@ -2,9 +2,16 @@ function User(email, name) {
 	this.email = email;
 	this.name = name;
 	this.online = false;
-	this.login = function() {
-		console.log(this.email, 'logged in');
-	}
+}
+
+User.prototype.login = function() {
+	this.online = true;
+	console.log(this.email, 'logged in');
+}
+
+User.prototype.logout = function() {
+	this.online = false;
+	console.log(this.email, 'logged out');
 }
 
 var userOne = new User('wiseholo@gmail.com', 'Holo');
