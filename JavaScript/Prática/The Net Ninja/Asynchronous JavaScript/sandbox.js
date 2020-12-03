07d1f13892ef898355f0e29fe4bcc670579795b1
Luís Galete <luisgalete6264@gmail.com>
2020-12-03 19:25:39 -0300

    Add lesson 5

--- JavaScript/Prática/The Net Ninja/Asynchronous JavaScript/sandbox.js
+++ JavaScript/Prática/The Net Ninja/Asynchronous JavaScript/sandbox.js
@@ -1,23 +1,24 @@
-const getTodos = (callback) => {
+const getBlades = (callback) => {
   const request = new XMLHttpRequest();
 
   request.addEventListener('readystatechange', () => {
     // console.log(request, request.readyState);
     if (request.readyState === 4 && request.status === 200) {
-      callback(undefined, request.responseText);
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
     } else if (request.readyState === 4) {
       callback('could not fetch data', undefined);
     }
   })
   
-  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+  request.open('GET', 'blades.json');
   request.send();
 };
 
 console.log(1);
 console.log(2);
 
-getTodos((err, data) => {
+getBlades((err, data) => {
   console.log('callback fired');
   if (err) {
     console.log(err);
