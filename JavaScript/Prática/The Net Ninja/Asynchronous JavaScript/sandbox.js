49a779525230d9f099f5f2a52f5880568ad7097e
Luís Galete <luisgalete6264@gmail.com>
2020-12-03 18:16:40 -0300

    Add lesson 2

---
+++ JavaScript/Prática/The Net Ninja/Asynchronous JavaScript/sandbox.js
@@ -0,0 +1,11 @@
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', () => {
+  // console.log(request, request.readyState);
+  if (request.readyState === 4) {
+    console.log(request.responseText)
+  }
+})
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+request.send();
