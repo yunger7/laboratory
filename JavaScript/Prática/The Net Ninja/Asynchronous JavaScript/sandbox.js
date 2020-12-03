c96fbe6a576bce106468f79d6a0f7fc229b6005a
Luís Galete <luisgalete6264@gmail.com>
2020-12-03 18:34:09 -0300

    Add lesson 3

--- JavaScript/Prática/The Net Ninja/Asynchronous JavaScript/sandbox.js
+++ JavaScript/Prática/The Net Ninja/Asynchronous JavaScript/sandbox.js
@@ -2,8 +2,10 @@ const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
   // console.log(request, request.readyState);
-  if (request.readyState === 4) {
-    console.log(request.responseText)
+  if (request.readyState === 4 && request.status === 200) {
+    console.log(request, request.responseText)
+  } else if (request.readyState === 4) {
+    console.log("Could not fetch the data");
   }
 })
 
