f975fcd6ba3d1589e0a2d5268dcd3339bbb574ec
Luís Galete <luisgalete6264@gmail.com>
2020-12-03 18:53:46 -0300

    Add lesson 4

--- JavaScript/Prática/The Net Ninja/Asynchronous JavaScript/sandbox.js
+++ JavaScript/Prática/The Net Ninja/Asynchronous JavaScript/sandbox.js
@@ -1,13 +1,30 @@
-const request = new XMLHttpRequest();
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', () => {
-  // console.log(request, request.readyState);
-  if (request.readyState === 4 && request.status === 200) {
-    console.log(request, request.responseText)
-  } else if (request.readyState === 4) {
-    console.log("Could not fetch the data");
+  request.addEventListener('readystatechange', () => {
+    // console.log(request, request.readyState);
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.responseText);
+    } else if (request.readyState === 4) {
+      callback('could not fetch data', undefined);
+    }
+  })
+  
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+  request.send();
+};
+
+console.log(1);
+console.log(2);
+
+getTodos((err, data) => {
+  console.log('callback fired');
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
   }
-})
+});
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
+console.log(3);
+console.log(4);
