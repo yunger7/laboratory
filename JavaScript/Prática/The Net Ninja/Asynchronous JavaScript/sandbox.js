cfef42351938e9005c31bd8ea9e54833dc3d88cb
Luís Galete <luisgalete6264@gmail.com>
2020-12-06 14:59:43 -0300

    Add lesson 10

--- JavaScript/Prática/The Net Ninja/Asynchronous JavaScript/sandbox.js
+++ JavaScript/Prática/The Net Ninja/Asynchronous JavaScript/sandbox.js
@@ -1,9 +1,23 @@
 // Fetch API
-fetch('data/blades.json').then(response => {
-  console.log('resolved', response);
-  return response.json();
-}).then(data => {
-  console.log(data);
-}).catch(err => {
-  console.log(err);
-})
+
+const getData = async () => {
+  var response = await fetch('data/blades.json');
+  const blades = await response.json();
+  var response = await fetch('data/drivers.json');
+  const drivers = await response.json();
+  return {blades, drivers};
+};
+
+getData().then(data => {
+  console.log(data.blades);
+  console.log(data.drivers);
+});
+
+// fetch('data/blades.json').then(response => {
+//   console.log('resolved', response);
+//   return response.json();
+// }).then(data => {
+//   console.log(data);
+// }).catch(err => {
+//   console.log(err);
+// })
