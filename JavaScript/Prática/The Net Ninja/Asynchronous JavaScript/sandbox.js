22d082720a2f4ea5e695d33674a82e1b574fc390
Luís Galete <luisgalete6264@gmail.com>
2020-12-06 14:31:06 -0300

    Add lesson 9

--- JavaScript/Prática/The Net Ninja/Asynchronous JavaScript/sandbox.js
+++ JavaScript/Prática/The Net Ninja/Asynchronous JavaScript/sandbox.js
@@ -1,28 +1,9 @@
-const getBlades = (resource) => {
-
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-
-    request.addEventListener('readystatechange', () => {
-      // console.log(request, request.readyState);
-      if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText);
-        resolve(data);
-      } else if (request.readyState === 4) {
-        reject('error getting resource');
-      }
-    })
-    
-    request.open('GET', resource);
-    request.send();
-  });
-};
-
-getBlades('data/drivers.json').then(data => {
-  console.log('promise 1 resolved:', data);
-  return getBlades('data/blades.json');
+// Fetch API
+fetch('data/blades.json').then(response => {
+  console.log('resolved', response);
+  return response.json();
 }).then(data => {
-  console.log('promise 2 resolved:', data);
+  console.log(data);
 }).catch(err => {
-  console.log('promise rejected:', err);
-});
+  console.log(err);
+})
