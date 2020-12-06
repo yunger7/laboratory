7606e98d3a1b3b2995c293c58f87c585c8465e10
Luís Galete <luisgalete6264@gmail.com>
2020-12-06 13:17:55 -0300

    Add lesson 7

--- JavaScript/Prática/The Net Ninja/Asynchronous JavaScript/sandbox.js
+++ JavaScript/Prática/The Net Ninja/Asynchronous JavaScript/sandbox.js
@@ -1,23 +1,25 @@
-const getBlades = (resource, callback) => {
-  const request = new XMLHttpRequest();
+const getBlades = (resource) => {
 
-  request.addEventListener('readystatechange', () => {
-    // console.log(request, request.readyState);
-    if (request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4) {
-      callback('could not fetch data', undefined);
-    }
-  })
-  
-  request.open('GET', resource);
-  request.send();
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+      // console.log(request, request.readyState);
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject('error getting resource');
+      }
+    })
+    
+    request.open('GET', resource);
+    request.send();
+  });
 };
 
-getBlades('data/blades.json', (err, data) => {
-  console.log(data);
-  getBlades('data/drivers.json', (err, data) => {
-    console.log(data);
-  });
+getBlades('data/drivers.json').then(data => {
+  console.log('promise resolved:', data);
+}).catch(err => {
+  console.log('promise rejected:', err);
 });
