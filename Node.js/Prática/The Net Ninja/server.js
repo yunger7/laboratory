4afec2b56f0dea4beaa5399bd5693e28fa450746
Luís Galete <luisgalete6264@gmail.com>
2020-12-09 18:36:21 -0300

    Add lesson 4

--- Node.js/Prática/The Net Ninja/server.js
+++ Node.js/Prática/The Net Ninja/server.js
@@ -1,12 +1,48 @@
-// Requiring the HTTP core module
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  // Runs every time we get a request
-  console.log('request made');
+  // runs every time we get a request
+  console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader('Content-Type', 'text/html');
+
+  // Handle path
+  let path = './views/';
+  switch(req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    case '/about-me':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end();
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+      break
+  }
+
+  // send a html page
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
-// Make the server listen for requests on port 3000
+// make the server listen for requests on port 3000
 server.listen(3000, 'localhost', () => {
   console.log('listening for requests on port 3000')
 })
