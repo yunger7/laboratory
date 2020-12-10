a0caeec2893cee98ec911309df10bd10e4489668
Luís Galete <luisgalete6264@gmail.com>
2020-12-10 15:46:36 -0300

    Add lesson 5

--- Node.js/Prática/The Net Ninja/server.js
+++ Node.js/Prática/The Net Ninja/server.js
@@ -1,9 +1,20 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
   // runs every time we get a request
-  console.log(req.url, req.method);
+  
+  // lodash
+  const num = _.random(0, 20);
+  console.log(num);
+
+  const greet = _.once(() => {
+    console.log('Hello :)');
+  });
+
+  greet();
+  greet();
 
   // set header content type
   res.setHeader('Content-Type', 'text/html');
