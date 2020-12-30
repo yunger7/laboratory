492fbae69066bae2de1c5fd16056446bcc0da9b7
Luís Galete <luisgalete6264@gmail.com>
2020-12-30 11:38:55 -0300

    Finish TNN Firestore course

--- Prática/Firebase/The Net Ninja/Firestore/app.js
+++ Prática/Firebase/The Net Ninja/Firestore/app.js
@@ -28,11 +28,13 @@ function renderCafe(doc) {
 }
 
 // get data
-db.collection('cafes').get().then(snapshot => {
-  snapshot.docs.forEach(doc => {
-    renderCafe(doc);
-  });
-});
+// db.collection('cafes').orderBy('city').get().then(snapshot => {
+//   snapshot.docs.forEach(doc => {
+//     renderCafe(doc);
+//   });
+// });
+
+
 
 // save data
 form.addEventListener('submit', e => {
@@ -45,4 +47,18 @@ form.addEventListener('submit', e => {
 
   form.name.value = '';
   form.city.value = '';
+});
+
+// real-time listener
+db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
+  let changes = snapshot.docChanges();
+
+  changes.forEach(change => {
+    if (change.type == "added") {
+      renderCafe(change.doc)
+    } else if (change.type == "removed") {
+      let li = cafeList.querySelector('li[data-id=' + change.doc.id  + ']');
+      cafeList.removeChild(li);
+    }
+  });
 });
