62fbd5b7ff2e965a2017a827191654fa142b5ed6
Luís Galete <luisgalete6264@gmail.com>
2021-01-06 17:05:38 -0300

    Add login, logout and signup

---
+++ Prática/Firebase/The Net Ninja/Auth/scripts/auth.js
@@ -0,0 +1,43 @@
+// signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  // signup user
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  });
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', e => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('user signed out');
+  });
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred);
+
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+});
