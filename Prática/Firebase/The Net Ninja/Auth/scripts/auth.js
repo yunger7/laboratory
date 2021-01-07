7bb04f7828f775c65b1acd7c64ad30a1c010e427
Luís Galete <luisgalete6264@gmail.com>
2021-01-07 19:33:46 -0300

    Add user user bio info

--- Prática/Firebase/The Net Ninja/Auth/scripts/auth.js
+++ Prática/Firebase/The Net Ninja/Auth/scripts/auth.js
@@ -4,7 +4,7 @@ auth.onAuthStateChanged(user => {
     db.collection('guides').onSnapshot(snapshot => {
       setupGuides(snapshot.docs);
       setupUI(user);
-    }).catch(err => {
+    }, err => {
       console.log(err.message);
     });
   } else {
@@ -42,6 +42,10 @@ signupForm.addEventListener('submit', e => {
 
   // signup user
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    return db.collection('users').doc(cred.user.uid).set({
+      bio: signupForm['signup-bio'].value
+    });
+  }).then(() => {
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
     signupForm.reset();
--- Prática/Firebase/The Net Ninja/Auth/scripts/index.js
+++ Prática/Firebase/The Net Ninja/Auth/scripts/index.js
@@ -6,10 +6,13 @@ const accountDetails = document.querySelector('.account-details');
 const setupUI = user => {
   if (user) {
     // account info
-    const html = `
-      <div>Logged in as ${user.email}</div>
-    `;
-    accountDetails.innerHTML = html;
+    db.collection('users').doc(user.uid).get().then(doc => {
+      const html = `
+        <div>Logged in as ${user.email}</div>
+        <div>${doc.data().bio}</div>
+      `;
+      accountDetails.innerHTML = html;
+    })
 
     // toggle UI elements
     loggedInLinks.forEach(item => item.style.display = 'block');
