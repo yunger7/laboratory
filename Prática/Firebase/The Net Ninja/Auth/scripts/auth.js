bedc7343bfa6fcb26596ad56d76454cb58f42c42
Luís Galete <luisgalete6264@gmail.com>
2021-01-07 14:26:53 -0300

    UI & Firestore security rules

--- Prática/Firebase/The Net Ninja/Auth/scripts/auth.js
+++ Prática/Firebase/The Net Ninja/Auth/scripts/auth.js
@@ -1,14 +1,11 @@
-// get data
-db.collection('guides').get().then(snapshot => {
-  setupGuides(snapshot.docs);
-})
-
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
   if (user) {
-    console.log('user logged in: ', user);
+    db.collection('guides').get().then(snapshot => {
+      setupGuides(snapshot.docs);
+    });
   } else {
-    console.log('user logged out');
+    setupGuides([]);
   }
 });
 
