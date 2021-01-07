20b30da88c965f3a34b239e59af7dd9d5adcd2d8
Luís Galete <luisgalete6264@gmail.com>
2021-01-07 15:44:03 -0300

    Add realtime listener

--- Prática/Firebase/The Net Ninja/Auth/scripts/auth.js
+++ Prática/Firebase/The Net Ninja/Auth/scripts/auth.js
@@ -1,7 +1,7 @@
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
   if (user) {
-    db.collection('guides').get().then(snapshot => {
+    db.collection('guides').onSnapshot(snapshot => {
       setupGuides(snapshot.docs);
       setupUI(user);
     });
