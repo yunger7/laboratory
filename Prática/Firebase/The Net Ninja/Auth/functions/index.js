c6e1adde8789db769f1b84f50a79954dc361be2a
Luís Galete <luisgalete6264@gmail.com>
2021-01-09 18:21:52 -0300

    Add security to addAdminRole() function

--- Prática/Firebase/The Net Ninja/Auth/functions/index.js
+++ Prática/Firebase/The Net Ninja/Auth/functions/index.js
@@ -4,6 +4,11 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.addAdminRole = functions.https.onCall((data, context) => {
+  // check if request is made by an admin
+  if (context.auth.token.admin !== true) {
+    return { error: 'Only admins can add other admin, haha' }
+  }
+
   // get user and add custom claim (admin)
   return admin.auth().getUserByEmail(data.email).then(user => {
     return admin.auth().setCustomUserClaims(user.uid, {
