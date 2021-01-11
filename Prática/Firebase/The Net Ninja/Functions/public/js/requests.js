ae1116331d96bdc7421b1ddab6b19c44ea3053a0
Luís Galete <luisgalete6264@gmail.com>
2021-01-11 20:32:11 -0300

    Add Vue instance

--- Prática/Firebase/The Net Ninja/Functions/public/js/requests.js
+++ Prática/Firebase/The Net Ninja/Functions/public/js/requests.js
@@ -1,8 +1,18 @@
-const ref = firebase.firestore().collection('requests');
-ref.onSnapshot(snapshot => {
-  let requests = [];
-  snapshot.forEach(doc => {
-    requests.push({...doc.data(), id: doc.id})
-  });
-  console.log(requests);
-});
+var app = new Vue({
+  el: '#app',
+  data: {
+    requests: []
+  },
+  mounted() {
+    const ref = firebase.firestore().collection('requests');
+
+    ref.onSnapshot(snapshot => {
+      let requests = [];
+      snapshot.forEach(doc => {
+        requests.push({...doc.data(), id: doc.id})
+      });
+      
+      this.requests = requests;
+    });
+  }
+})
