aec9324d413093a832ef3858d455531568958850
Luís Galete <luisgalete6264@gmail.com>
2021-01-11 19:53:37 -0300

    Add requests (front-end)

--- Prática/Firebase/The Net Ninja/Functions/public/js/app.js
+++ Prática/Firebase/The Net Ninja/Functions/public/js/app.js
@@ -1,5 +1,6 @@
 const requestModal = document.querySelector('.new-request');
 const requestLink = document.querySelector('.add-request');
+const requestForm = document.querySelector('.new-request form');
 
 // open request modal
 requestLink.addEventListener('click', () => {
@@ -11,6 +12,23 @@ requestModal.addEventListener('click', (e) => {
   if (e.target.classList.contains('new-request')) {
     requestModal.classList.remove('open');
   }
+});
+
+// add a new request
+requestForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const addRequest = firebase.functions().httpsCallable('addRequest');
+  addRequest({
+    text: requestForm.request.value,
+  }).then(() => {
+    requestForm.reset();
+    requestForm.querySelector('.error').textContent = '';
+    requestModal.classList.remove('open');
+  })
+  .catch(err => {
+    requestForm.querySelector('.error').textContent = err.message;
+  });
 });
 
 // sayHello function call
