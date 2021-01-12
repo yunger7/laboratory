5fdc9247416bf9b501ffb35dfb023784e7d62e02
Luís Galete <luisgalete6264@gmail.com>
2021-01-12 17:12:16 -0300

    Add Firestore trigger test function

--- Prática/Firebase/The Net Ninja/Functions/functions/index.js
+++ Prática/Firebase/The Net Ninja/Functions/functions/index.js
@@ -89,3 +89,26 @@ exports.upvote = functions.https.onCall(async (data, context) => {
     upvotes: admin.firestore.FieldValue.increment(1),
   });
 });
+
+// firestore trigger for tracking activity
+exports.logActivities = functions.firestore.document("/{collection}/{id}")
+  .onCreate((snap, context) => {
+    console.log(snap.data());
+
+    const collection = context.params.collection;
+    const id = context.params.id;
+
+    const activities = admin.firestore().collection("activities");
+
+    if (collection === "requests") {
+      return activities.add({ text: "a new request was added" });
+    }
+
+    if (collection === "users") {
+      return activities.add({ text: "a new user signed up" });
+    }
+
+    return null;
+
+  }
+);
