3a2f82f05806d1db4f15b8bea16f293e4fcec9e1
Luís Galete <luisgalete6264@gmail.com>
2021-01-13 18:43:44 -0300

    Click events

--- Prática/React/The Net Ninja/dojo-blog/src/Home.js
+++ Prática/React/The Net Ninja/dojo-blog/src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+  const handleClick = () => {
+    console.log("Thanks for clicking");
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log("Hello " + name, e.target);
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain("Luigi", e)}>Click me again</button>
     </div>
   );
 }
