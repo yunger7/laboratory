c354c8449b69a542cd22b77bd56d0b7e053fcf87
Luís Galete <luisgalete6264@gmail.com>
2021-01-13 19:16:25 -0300

    Use state

--- Prática/React/The Net Ninja/dojo-blog/src/Home.js
+++ Prática/React/The Net Ninja/dojo-blog/src/Home.js
@@ -1,17 +1,21 @@
-const Home = () => {
-  const handleClick = () => {
-    console.log("Thanks for clicking");
-  }
+import { useState } from 'react';
 
-  const handleClickAgain = (name, e) => {
-    console.log("Hello " + name, e.target);
+const Home = () => {
+  // let name = "Mario";
+  const [name, setName] = useState("Mario");
+  const [age, setAge] = useState(18);
+
+  const handleClick = () => {
+    setName("Luigi");
+    setAge(20);
   }
 
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>Name: { name }</p>
+      <p>Age: { age }</p>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain("Luigi", e)}>Click me again</button>
     </div>
   );
 }
