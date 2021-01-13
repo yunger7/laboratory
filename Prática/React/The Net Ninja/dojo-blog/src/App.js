4a4456d02c1d87d6877d093b0b49b93f63ee6697
Luís Galete <luisgalete6264@gmail.com>
2021-01-13 11:54:36 -0300

    Dynamic values in templates

--- Prática/React/The Net Ninja/dojo-blog/src/App.js
+++ Prática/React/The Net Ninja/dojo-blog/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the blog!";
+  const link = "https://www.google.com";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App component</h1>
+        <h1>{ title }</h1>
+
+        <p>{ 10 }</p>
+        <p>{ "Hello world!" }</p>
+        <p>{ [1, 2, 3, 4] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        <a href={ link }>Google website</a>
       </div>
     </div>
   );
