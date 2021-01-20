a849e7bac4fa734df9e266241487efe341374224
Luís Galete <luisgalete6264@gmail.com>
2021-01-20 18:54:48 -0300

    Basics of useEffect hook

--- Prática/React/The Net Ninja/dojo-blog/src/App.js
+++ Prática/React/The Net Ninja/dojo-blog/src/App.js
@@ -2,9 +2,6 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = "Welcome to the blog!";
-  const link = "https://www.google.com";
-
   return (
     <div className="App">
       <Navbar />
