2cb67e0a996427f1a622c923da58d17412f4b17a
Luís Galete <luisgalete6264@gmail.com>
2021-01-20 19:10:47 -0300

    useEffect dependencies

--- Prática/React/The Net Ninja/dojo-blog/src/Home.js
+++ Prática/React/The Net Ninja/dojo-blog/src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
     {title: "Creating an AI with python", body: "Lorem ipsum...", author: "Ryuunosuke", id: 3}
   ]);
 
+  const [name, setName] = useState('Nino');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
@@ -15,13 +17,15 @@ const Home = () => {
 
   useEffect(() => {
     console.log('use effect ran');
-    console.log(blogs);
-  });
+    console.log(name);
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
       <BlogList blogs={blogs.filter((blog) => blog.author === "Rin")} title="Rin's Blogs" />
+      <button onClick={() => setName('Miku')}>Change name</button>
+      <p>{ name }</p>
     </div>
   );
 }
