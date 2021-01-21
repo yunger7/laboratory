0350a6a93fcb812c3a909f70ac91c90ec8fd3cef
Luís Galete <luisgalete6264@gmail.com>
2021-01-21 20:15:28 -0300

    Error handling

--- Prática/React/sandbox/src/Posts.js
+++ Prática/React/sandbox/src/Posts.js
@@ -3,6 +3,8 @@ import PostsList from './PostsList';
 
 const Posts = () => {
   const [posts, setPosts] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleDelete = (id) => {
     const newPosts = posts.filter(post => post.id !== id);
@@ -18,9 +20,12 @@ const Posts = () => {
       const data = await res.json();
       const posts = data.slice(0, 10);
       setPosts(posts);
+      setError(null);
+      setIsLoading(false);
     }
     catch (err) {
-      console.log(err.message);
+      setError(err.message);
+      setIsLoading(false);
     }
   }
 
@@ -30,6 +35,8 @@ const Posts = () => {
   
   return (
     <div className="posts">
+      {error && <div className="error">{ error }</div>}
+      {isLoading && <div className="loading">Loading...</div>}
       {posts && <PostsList posts={posts} handleDelete={handleDelete} />}
     </div>
   );
