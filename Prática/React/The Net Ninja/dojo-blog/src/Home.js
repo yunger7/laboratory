5390dc0b21f883122e689900d1a12132a1788931
Luís Galete <luisgalete6264@gmail.com>
2021-01-21 18:34:13 -0300

    Conditional loading message

--- Prática/React/The Net Ninja/dojo-blog/src/Home.js
+++ Prática/React/The Net Ninja/dojo-blog/src/Home.js
@@ -3,17 +3,22 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     console.log('use effect ran');
 
     fetch('http://localhost:8000/blogs')
     .then(res => res.json())
-    .then(data => setBlogs(data));
+    .then(data => {
+      setBlogs(data);
+      setIsLoading(false);
+    });
   }, []);
 
   return (
     <div className="home">
+      {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs" />}
       {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === "Rin")} title="Rin's Blogs" />}
     </div>
