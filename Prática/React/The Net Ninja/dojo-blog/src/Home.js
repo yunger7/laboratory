b68c306f104d72a0096d3f34672f710d5e291c07
Luís Galete <luisgalete6264@gmail.com>
2021-01-21 18:54:09 -0300

    Handling fetch errors

--- Prática/React/The Net Ninja/dojo-blog/src/Home.js
+++ Prática/React/The Net Ninja/dojo-blog/src/Home.js
@@ -4,20 +4,32 @@ import BlogList from './BlogList';
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     console.log('use effect ran');
 
     fetch('http://localhost:8000/blogs')
-    .then(res => res.json())
+    .then(res => {
+      if (!res.ok) {
+        throw Error('could not fetch data');
+      }
+      return res.json()
+    })
     .then(data => {
       setBlogs(data);
+      setIsLoading(false);
+      setError(null);
+    })
+    .catch(err => {
+      setError(err.message);
       setIsLoading(false);
     });
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{ error }</div>}
       {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs" />}
       {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === "Rin")} title="Rin's Blogs" />}
