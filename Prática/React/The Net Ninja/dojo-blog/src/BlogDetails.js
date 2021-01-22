2a39fb24aeadfd126f1e44a96d98c01aca3ff937
Luís Galete <luisgalete6264@gmail.com>
2021-01-22 18:16:49 -0300

    Reusing custom hooks

--- Prática/React/The Net Ninja/dojo-blog/src/BlogDetails.js
+++ Prática/React/The Net Ninja/dojo-blog/src/BlogDetails.js
@@ -1,11 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFetch from './useFetch';
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const { data: blog, isLoading, error} = useFetch(`http://localhost:8000/blogs/${id}`);
 
   return (
     <div className="blog-details">
-      <h2>Blog details - { id }</h2>
+      {error && <div>{ error }</div>}
+      {isLoading && <div>Loading...</div>}
+      {blog && (
+        <article>
+          <h2>{ blog.title }</h2>
+          <p>Written by: { blog.author }</p>
+          <div>{ blog.body }</div>
+        </article>
+      )}
     </div>
   );
 }
