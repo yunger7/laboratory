11d7f055430d42754890a7a4854e575b24485c1d
Luís Galete <luisgalete6264@gmail.com>
2021-01-22 18:36:50 -0300

    Controlled inputs

--- Prática/React/The Net Ninja/dojo-blog/src/Create.js
+++ Prática/React/The Net Ninja/dojo-blog/src/Create.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('rin')
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={ title }
+          onChange={e => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={ body }
+          onChange={e => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog Author:</label>
+        <select
+          value={ author }
+          onChange={e => setAuthor(e.target.value)}
+        >
+          <option value="rin">Rin</option>
+          <option value="holo">Holo</option>
+        </select>
+        <button>Add blog</button>
+      </form>
     </div>
   );
 }
