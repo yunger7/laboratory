694783c31b5d83671518cde8cdd103f2fafaa982
Luís Galete <luisgalete6264@gmail.com>
2021-01-22 18:47:34 -0300

    Submit events

--- Prática/React/The Net Ninja/dojo-blog/src/Create.js
+++ Prática/React/The Net Ninja/dojo-blog/src/Create.js
@@ -5,10 +5,17 @@ const Create = () => {
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('rin')
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author }
+
+    console.log(blog);
+  }
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
