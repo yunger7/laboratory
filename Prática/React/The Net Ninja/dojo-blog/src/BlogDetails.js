72b72ee88595603c6bb4235d4ea8fd37b5512179
Luís Galete <luisgalete6264@gmail.com>
2021-01-22 19:29:43 -0300

    Delete request

--- Prática/React/The Net Ninja/dojo-blog/src/BlogDetails.js
+++ Prática/React/The Net Ninja/dojo-blog/src/BlogDetails.js
@@ -1,9 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
   const { id } = useParams();
   const { data: blog, isLoading, error} = useFetch(`http://localhost:8000/blogs/${id}`);
+  const history = useHistory();
+
+  const handleDelete = () => {
+    fetch(`http://localhost:8000/blogs/${blog.id}`, {
+      method: 'DELETE'
+    })
+    .then(() => {
+      history.push('/');
+    });
+  }
 
   return (
     <div className="blog-details">
@@ -14,6 +24,7 @@ const BlogDetails = () => {
           <h2>{ blog.title }</h2>
           <p>Written by: { blog.author }</p>
           <div>{ blog.body }</div>
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
