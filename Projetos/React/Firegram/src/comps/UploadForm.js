1d7b3e525716bdea57f795cb29985124cf9d5aff
Luís Galete <luisgalete6264@gmail.com>
2021-01-25 19:15:43 -0300

    Create useStorage hook

--- Projetos/React/Firegram/src/comps/UploadForm.js
+++ Projetos/React/Firegram/src/comps/UploadForm.js
@@ -20,7 +20,10 @@ const UploadForm = () => {
 
   return (
     <form>
-      <input type="file" onChange={changeHandler} />
+      <label>
+        <input type="file" onChange={changeHandler} />
+        <span>+</span>
+      </label>
       <div className="output">
         { error && <div className="error">{ error }</div> }
         { error && <div>{ file.name }</div> }
