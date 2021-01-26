81458f13a52fc4749158631cac080bea2abbe5b8
Luís Galete <luisgalete6264@gmail.com>
2021-01-26 09:47:06 -0300

    Misspell 'animate'

--- Projetos/React/Firegram/src/comps/ProgressBar.js
+++ Projetos/React/Firegram/src/comps/ProgressBar.js
@@ -14,7 +14,7 @@ const ProgressBar = ({ file, setFile }) => {
   return (
     <motion.div className="progress-bar"
       initial={{ width: 0 }}
-      animte={{ width: progress + '%' }}
+      animate={{ width: progress + '%' }}
     ></motion.div>
   )
 }
