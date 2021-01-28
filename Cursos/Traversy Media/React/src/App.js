bb0685a1cc5a4b02fd6b70fe27a75a469e3c37db
Luís Galete <luisgalete6264@gmail.com>
2021-01-28 12:25:56 -0300

    Add task submit handler

--- Cursos/Traversy Media/React/src/App.js
+++ Cursos/Traversy Media/React/src/App.js
@@ -1,47 +1,62 @@
-import { useState } from 'react';
-import Header from './components/Header';
-import Tasks from './components/Tasks';
-import AddTask from './components/AddTask';
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Clean my room",
-      day: "Jan 28 at 10:00 AM",
-      reminder: false,
-    },
-    {
-      id: 2,
-      text: "Buy groceries",
-      day: "Jan 28 at 2:00 PM",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Meditate",
-      day: "Jan 28 at 10:00 PM",
-      reminder: true,
-    }
-  ]);
+	const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			text: "Clean my room",
+			day: "Jan 28 at 10:00 AM",
+			reminder: false,
+		},
+		{
+			id: 2,
+			text: "Buy groceries",
+			day: "Jan 28 at 2:00 PM",
+			reminder: true,
+		},
+		{
+			id: 3,
+			text: "Meditate",
+			day: "Jan 28 at 10:00 PM",
+			reminder: true,
+		},
+	]);
 
-  // delete task
-  const deleteTask = (id) => {
-    setTasks(tasks.filter(task => task.id !== id));
-  }
+	// add task
+	const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask])
+	};
 
-  // toggle reminder
-  const toggleReminder = (id) => {
-    setTasks(tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder } : task))
-  }
+	// delete task
+	const deleteTask = (id) => {
+		setTasks(tasks.filter((task) => task.id !== id));
+	};
 
-  return (
-    <div className="container">
-      <Header />
-      <AddTask />
-      { tasks.length ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "No tasks" }
-    </div>
-  );
+	// toggle reminder
+	const toggleReminder = (id) => {
+		setTasks(
+			tasks.map((task) =>
+				task.id === id ? { ...task, reminder: !task.reminder } : task
+			)
+		);
+	};
+
+	return (
+		<div className="container">
+			<Header />
+			<AddTask onAdd={addTask} />
+			{tasks.length ? (
+				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+			) : (
+				"No tasks"
+			)}
+		</div>
+	);
 }
 
 export default App;
--- Cursos/Traversy Media/React/src/components/AddTask.js
+++ Cursos/Traversy Media/React/src/components/AddTask.js
@@ -1,12 +1,27 @@
 import { useState } from "react";
 
-const AddTask = () => {
+const AddTask = ({ onAdd }) => {
 	const [text, setText] = useState("");
 	const [day, setDay] = useState("");
 	const [reminder, setReminder] = useState(false);
 
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		if (!text) {
+			alert("Please add a task");
+			return;
+		}
+
+		onAdd({ text, day, reminder });
+
+		setText("");
+		setDay("");
+		setReminder(false);
+	};
+
 	return (
-		<form className="add-form">
+		<form onSubmit={onSubmit} className="add-form">
 			<div className="form-control">
 				<label>Task</label>
 				<input
@@ -27,7 +42,12 @@ const AddTask = () => {
 			</div>
 			<div className="form-control form-control-check">
 				<label>Set Reminder</label>
-				<input type="checkbox" value={reminder} onChange={e => setReminder(e.currentTarget.checked)} />
+				<input
+          type="checkbox"
+          checked={reminder}
+					value={reminder}
+					onChange={(e) => setReminder(e.currentTarget.checked)}
+				/>
 			</div>
 
 			<input type="submit" value="Save task" className="btn btn-block" />
