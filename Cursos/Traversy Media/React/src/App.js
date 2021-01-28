e562b74e8a5c8c155dba1f88ad2dad9b3e8fa211
Luís Galete <luisgalete6264@gmail.com>
2021-01-28 12:03:24 -0300

    Toggle reminder & Conditional styling

--- Cursos/Traversy Media/React/src/App.js
+++ Cursos/Traversy Media/React/src/App.js
@@ -29,10 +29,15 @@ function App() {
     setTasks(tasks.filter(task => task.id !== id));
   }
 
+  // toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
     <div className="container">
       <Header />
-      { tasks.length ? <Tasks tasks={tasks} onDelete={deleteTask} /> : "No tasks"}
+      { tasks.length ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "No tasks" }
     </div>
   );
 }
--- Cursos/Traversy Media/React/src/components/Task.js
+++ Cursos/Traversy Media/React/src/components/Task.js
@@ -1,8 +1,8 @@
 import { FaTimes } from 'react-icons/fa';
 
-const Task = ({ task, onDelete }) => {
+const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className="task">
+    <div className={`task ${ task.reminder ? 'reminder' : '' }`} onDoubleClick={() => onToggle(task.id)}>
       <h3>{ task.text } <FaTimes onClick={() => onDelete(task.id)} style={{ cursor: 'pointer' }} /></h3>
       <p>{ task.day }</p>
     </div>
