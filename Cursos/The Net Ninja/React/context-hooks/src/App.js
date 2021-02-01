79a259fee2cc10ace0c9be3f8d210548eba43262
Luís Galete <luisgalete6264@gmail.com>
2021-02-01 11:28:23 -0300

    Create context

--- Cursos/The Net Ninja/React/context-hooks/src/App.js
+++ Cursos/The Net Ninja/React/context-hooks/src/App.js
@@ -1,11 +1,14 @@
 import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }
