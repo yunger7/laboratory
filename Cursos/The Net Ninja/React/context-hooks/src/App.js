e6d5f1ed1b65c39a57db8eb109e29e5836ef0403
Luís Galete <luisgalete6264@gmail.com>
2021-02-01 15:22:09 -0300

    Create multiple contexts

--- Cursos/The Net Ninja/React/context-hooks/src/App.js
+++ Cursos/The Net Ninja/React/context-hooks/src/App.js
@@ -1,15 +1,18 @@
 import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
 import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./contexts/AuthContext";
 import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-        <ThemeToggle />
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
