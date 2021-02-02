a991f211a0803ba2882f80b294d644555b64eb90
Luís Galete <luisgalete6264@gmail.com>
2021-02-02 08:58:30 -0300

    Fix folder name conflict

---
+++ Cursos/The Net Ninja/React/Context and Hooks/hooksapp/src/App.js
@@ -0,0 +1,11 @@
+import SongList from "./components/SongList";
+
+function App() {
+  return (
+    <div className="App">
+      <SongList />
+    </div>
+  );
+}
+
+export default App;
---
+++ Cursos/The Net Ninja/React/Context and Hooks/hooksapp/src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongList = () => {
+  return (
+    <div className="song-list">
+      <ul>
+        <li>The wild darkness</li>
+        <li>Memory Gospel</li>
+      </ul>
+    </div>
+  );
+}
+ 
+export default SongList;
