00280d36ab658a8ba75a58f4ccaf6681d11571b7
Luís Galete <luisgalete6264@gmail.com>
2021-02-02 09:25:16 -0300

    useState hook

--- Cursos/The Net Ninja/React/Context and Hooks/hooksapp/src/components/SongList.js
+++ Cursos/The Net Ninja/React/Context and Hooks/hooksapp/src/components/SongList.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid'
 
 const SongList = () => {
+  const [songs, setSongs] = useState([
+    {id: 1, title: "Almost home"},
+    {id: 2, title: "Memory Gospel"},
+    {id: 3, title: "The wild darkness"}
+  ]);
+
+  const addSong = () => {
+    setSongs([...songs, { id: uuidv4(), title: "New song" }])
+  }
+
   return (
     <div className="song-list">
       <ul>
-        <li>The wild darkness</li>
-        <li>Memory Gospel</li>
+        {songs.map(song => (
+          <li key={song.id}>{ song.title }</li>
+        ))}
       </ul>
+      <button onClick={addSong}>Add song</button>
     </div>
   );
 }
