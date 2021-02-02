7de0c065f8c57f0d0045e7ad7de133531a99e65c
Luís Galete <luisgalete6264@gmail.com>
2021-02-02 13:42:36 -0300

    Add Navbar

--- Projetos/React/booklist/src/App.js
+++ Projetos/React/booklist/src/App.js
@@ -1,7 +1,12 @@
+import NavBar from "./components/NavBar";
+import BookContextProvider from "./contexts/BookContext";
+
 function App() {
   return (
     <div className="App">
-      
+      <BookContextProvider>
+        <NavBar />    
+      </BookContextProvider>
     </div>
   );
 }
