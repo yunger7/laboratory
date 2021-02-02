f42cde75eb2c62ae92e1c3f79500ec90e59f75b2
Luís Galete <luisgalete6264@gmail.com>
2021-02-02 11:05:05 -0300

    Hooks with context

--- Cursos/The Net Ninja/React/Context and Hooks/contextapp/src/components/BookList.js
+++ Cursos/The Net Ninja/React/Context and Hooks/contextapp/src/components/BookList.js
@@ -1,22 +1,37 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-  static contextType = ThemeContext;
+// class BookList extends Component {
+//   static contextType = ThemeContext;
 
-  render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
-        <ul>
-          <li style={{ background: theme.ui }}>The way of kings</li>
-          <li style={{ background: theme.ui }}>The name of the wind</li>
-          <li style={{ background: theme.ui }}>The final empire</li>
-        </ul>
-      </div>
-    );
-  }
+//   render() {
+//     const { isLightTheme, light, dark } = this.context;
+//     const theme = isLightTheme ? light : dark;
+//     return (
+//       <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+//         <ul>
+//           <li style={{ background: theme.ui }}>The way of kings</li>
+//           <li style={{ background: theme.ui }}>The name of the wind</li>
+//           <li style={{ background: theme.ui }}>The final empire</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+
+  return (
+    <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+      <ul>
+        <li style={{ background: theme.ui }}>The way of kings</li>
+        <li style={{ background: theme.ui }}>The name of the wind</li>
+        <li style={{ background: theme.ui }}>The final empire</li>
+      </ul>
+    </div>
+  );
 }
  
 export default BookList;
