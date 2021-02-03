45e78883f23a7a9c4efba9ea8ec8656e0f819a1a
Luís Galete <luisgalete6264@gmail.com>
2021-02-03 09:21:28 -0300

    Add base project

---
+++ Projetos/React/react-todo-app/src/App.js
@@ -0,0 +1,14 @@
+import { Button } from '@material-ui/core';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  );
+}
+
+export default App;
