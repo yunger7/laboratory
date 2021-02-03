153abedc9662875f82e305257cc930f4231ad43f
Luís Galete <luisgalete6264@gmail.com>
2021-02-03 11:31:11 -0300

    AddTask save to firestore

--- Projetos/React/react-todo-app/src/components/AddTask.jsx
+++ Projetos/React/react-todo-app/src/components/AddTask.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react';
+import { Firestore } from '../firebase/config';
 
 const AddTask = () => {
   const [task, setTask] = useState('');
+  const [loading, setLoading] = useState(false);
+
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    setLoading(true);
+
+    await Firestore.collection('tasks').add({task});
+    setTask('');
+    setLoading(false);
+  }
 
   return (
-    <form>
-      <input type="text" value={task} onChange={e => setTask(e.target.value)} placeholder="Task name" required />
-      <input type="submit" value="Save" />
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={task} onChange={e => setTask(e.target.value)} placeholder="Task name" disabled={loading} required />
+      <input type="submit" value="Save" disabled={loading} />
     </form>
   );
 }
