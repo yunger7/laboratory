47030bcf77d2098710e0857c0fd9d4e4210609e7
Luís Galete <luisgalete6264@gmail.com>
2021-02-05 18:35:10 -0300

    Test Buttons, inputs, checkboxes

---
+++ Experimentos/React/2/src/App.js
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import Public from '@material-ui/icons/Public';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import TextField from '@material-ui/core/TextField';
+
+const CheckboxExample = () => {
+  const [checked, setChecked] = useState(true);
+
+  return (
+    <div>
+      <FormControlLabel
+        control={
+          <Checkbox 
+            checked={checked}
+            onChange={e => setChecked(e.target.checked)}
+            color="primary"
+          />
+        }
+        label="I am a checkbox label"
+      />
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TextField
+          label="Email"
+          placeholder="example@test.com"
+        />
+        <CheckboxExample />
+        <ButtonGroup>
+          <Button
+            startIcon={<Public />}
+            size="large"
+            variant="contained"
+            color="primary"
+          >Hello World</Button>
+          <Button
+            endIcon={<Public />}
+            size="large"
+            variant="text"
+            color="secondary"
+          >Bye World</Button>
+        </ButtonGroup>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
