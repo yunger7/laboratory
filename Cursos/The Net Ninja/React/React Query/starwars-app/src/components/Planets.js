e9750e142d9f16c74a7cbe8638e370d0bb4a45c0
Luís Galete <luisgalete6264@gmail.com>
2021-02-05 10:37:43 -0300

    Query variables

--- Cursos/The Net Ninja/React/React Query/starwars-app/src/components/Planets.js
+++ Cursos/The Net Ninja/React/React Query/starwars-app/src/components/Planets.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import Planet from './Planet';
 
-const fetchPlanets = async () => {
-  const res = await fetch('http://swapi.dev/api/planets/');
+const fetchPlanets = async (key, string, page) => {
+  console.log(string);
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
   return res.json();
 }
 
 const Planets = () => {
-  const { data, status } = useQuery('planets', fetchPlanets);
+  const [page, setPage] = useState(1);
+  const { data, status } = useQuery(['planets', 'hello world', page], fetchPlanets);
   console.log(data);
 
   return (
     <div>
       <h2>Planets</h2>
+
+      <button onClick={() => setPage(1)}>Page 1</button>
+      <button onClick={() => setPage(2)}>Page 2</button>
+      <button onClick={() => setPage(3)}>Page 3</button>
 
       {status === 'loading' && (
         <div>Loading data</div>
