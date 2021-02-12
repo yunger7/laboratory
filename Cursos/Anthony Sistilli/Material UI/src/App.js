90ec2747a9b71ed7b8d794f17ad054c0739d2da8
Luís Galete <luisgalete6264@gmail.com>
2021-02-12 19:11:37 -0300

    Grid

--- Cursos/Anthony Sistilli/Material UI/src/App.js
+++ Cursos/Anthony Sistilli/Material UI/src/App.js
@@ -1,39 +1,18 @@
-import { useState } from 'react';
-import { Switch, Button, Typography, makeStyles, createMuiTheme, ThemeProvider, Paper } from "@material-ui/core";
-import CoolButton from "./CoolButton";
+import {createMuiTheme, ThemeProvider } from "@material-ui/core";
+import CenteredGrid from "./Grids";
 
-// const useStyles = makeStyles(theme => ({
-//   title: {
-//     fontStyle: 'oblique',
-//     fontSize: 24,
-//   },
-//   buttonStyle: {
-//     backgroundColor: 'blue',
-//     [theme.breakpoints.up("sm")]: {
-//       backgroundColor: 'black'
-//     }
-//   }
-// }))
+const theme = createMuiTheme({
+	palette: {
+		type: "dark",
+	},
+});
 
 function App() {
-  // const classes = useStyles();
-  const [darkTheme, setDarkTheme] = useState();
-
-  const theme = createMuiTheme({
-    palette: {
-      type: darkTheme ? "dark" : "light"
-    }
-  })
-
-  return (
-    <ThemeProvider theme={theme}>
-      <Paper style={{ height: '100vh' }} square>
-        <Typography variant="h1">Hellow orldd!</Typography>
-        <Button color="primary" variant="contained">Click me!</Button>
-        <Switch color="primary" checked={darkTheme} onChange={() => setDarkTheme(!darkTheme)} />
-      </Paper>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+      <CenteredGrid />
+		</ThemeProvider>
+	);
 }
 
 export default App;
