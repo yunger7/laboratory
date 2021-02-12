12397c0e65e6a5c1535c45427ab0fe5bc7ee143f
Luís Galete <luisgalete6264@gmail.com>
2021-02-12 20:07:23 -0300

    Card + Grid

--- Cursos/Anthony Sistilli/Material UI/src/App.js
+++ Cursos/Anthony Sistilli/Material UI/src/App.js
@@ -1,17 +1,36 @@
-import {createMuiTheme, ThemeProvider } from "@material-ui/core";
-import CenteredGrid from "./Grids";
+import { Grid, makeStyles } from "@material-ui/core";
+import OutlinedCard from "./Card";
 
-const theme = createMuiTheme({
-	palette: {
-		type: "dark",
-	},
-});
+const useStyles = makeStyles({
+  gridContainer: {
+    padding: '16px 30px'
+  }
+})
 
 function App() {
+  const classes = useStyles();
+
 	return (
-		<ThemeProvider theme={theme}>
-      <CenteredGrid />
-		</ThemeProvider>
+    <Grid container spacing={3} justify="space-around" className={classes.gridContainer}>
+      <Grid item xs={12} sm={6} md={4}>
+        <OutlinedCard />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <OutlinedCard />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <OutlinedCard />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <OutlinedCard />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <OutlinedCard />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <OutlinedCard />
+      </Grid>
+    </Grid>
 	);
 }
 
