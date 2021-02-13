7003b067bbf550eb58fc39d6053d9bb5ba662665
Luís Galete <luisgalete6264@gmail.com>
2021-02-13 13:05:08 -0300

    Tables

--- Cursos/Anthony Sistilli/Material UI/src/App.js
+++ Cursos/Anthony Sistilli/Material UI/src/App.js
@@ -1,36 +1,8 @@
-import { Grid, makeStyles } from "@material-ui/core";
-import OutlinedCard from "./Card";
-
-const useStyles = makeStyles({
-  gridContainer: {
-    padding: '16px 30px'
-  }
-})
+import BasicTable from "./Table";
 
 function App() {
-  const classes = useStyles();
-
 	return (
-    <Grid container spacing={3} justify="space-around" className={classes.gridContainer}>
-      <Grid item xs={12} sm={6} md={4}>
-        <OutlinedCard />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <OutlinedCard />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <OutlinedCard />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <OutlinedCard />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <OutlinedCard />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <OutlinedCard />
-      </Grid>
-    </Grid>
+    <BasicTable />
 	);
 }
 
