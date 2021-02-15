f911a101b5e11e4d4721063692ca6763198e9f8a
Luís Galete <luisgalete6264@gmail.com>
2021-02-15 11:06:09 -0300

    getStaticProps

--- Cursos/The Net Ninja/Next.js/ninjalist/pages/ninjas/index.js
+++ Cursos/The Net Ninja/Next.js/ninjalist/pages/ninjas/index.js
@@ -1,6 +1,26 @@
-const Ninjas = () => {
+import styles from "../../styles/Ninjas.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data }
+  }
+}
+
+const Ninjas = ({ ninjas }) => {
   return (
-    <h1>All Ninjas</h1>
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map(ninja => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ ninja.name }</h3>
+          </a>
+        </div>
+      ))}
+    </div>
   );
 }
  
