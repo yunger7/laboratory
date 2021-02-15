d1c9a9c5fc2d1dfa07725e0d0e6fd80d1979f63d
Luís Galete <luisgalete6264@gmail.com>
2021-02-15 09:47:54 -0300

    CSS Modules

--- Cursos/The Net Ninja/Next.js/ninjalist/pages/index.js
+++ Cursos/The Net Ninja/Next.js/ninjalist/pages/index.js
@@ -5,20 +5,20 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
 	return (
 		<div>
-			<h1>Homepage</h1>
-			<p>
+			<h1 className={styles.title}>Homepage</h1>
+			<p className={styles.text}>
 				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
 				possimus excepturi pariatur nemo consequatur veniam provident hic
 				eveniet expedita nesciunt, recusandae, velit illo maiores quam ipsum
 				sapiente modi est sunt.
 			</p>
-			<p>
+			<p className={styles.text}>
 				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
 				possimus excepturi pariatur nemo consequatur veniam provident hic
 				eveniet expedita nesciunt, recusandae, velit illo maiores quam ipsum
 				sapiente modi est sunt.
 			</p>
-      <Link href="/ninjas"><a>See Ninja Listing</a></Link>
+      <Link href="/ninjas"><a className={styles.btn}>See Ninja Listing</a></Link>
 		</div>
 	);
 }
