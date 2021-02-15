9fc34c555c04ef9c671f722fdf52953919c3cde2
Luís Galete <luisgalete6264@gmail.com>
2021-02-15 11:50:46 -0300

    Dynamic Routes

---
+++ Cursos/The Net Ninja/Next.js/ninjalist/pages/ninjas/[id].js
@@ -0,0 +1,9 @@
+const Details = () => {
+  return (
+    <div>
+      <h1>Details page</h1>
+    </div>
+  );
+}
+ 
+export default Details;
--- Cursos/The Net Ninja/Next.js/ninjalist/pages/ninjas/index.js
+++ Cursos/The Net Ninja/Next.js/ninjalist/pages/ninjas/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "../../styles/Ninjas.module.css";
 
 export const getStaticProps = async () => {
@@ -14,11 +15,11 @@ const Ninjas = ({ ninjas }) => {
     <div>
       <h1>All Ninjas</h1>
       {ninjas.map(ninja => (
-        <div key={ninja.id}>
+        <Link key={ninja.id} href={`/ninjas/${ninja.id}`}>
           <a className={styles.single}>
             <h3>{ ninja.name }</h3>
           </a>
-        </div>
+        </Link>
       ))}
     </div>
   );
