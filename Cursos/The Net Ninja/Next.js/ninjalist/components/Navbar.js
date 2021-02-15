7bc5607a39c0001eb4e2048facade92fd3931728
Luís Galete <luisgalete6264@gmail.com>
2021-02-15 10:46:43 -0300

    Redirecting & Metadata

--- Cursos/The Net Ninja/Next.js/ninjalist/components/Navbar.js
+++ Cursos/The Net Ninja/Next.js/ninjalist/components/Navbar.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
 	return (
 		<nav>
 			<div className="logo">
-				<h1>Ninjalist</h1>
+				<Image src="/logo.png" width={128} height={77} />
 			</div>
 			<Link href="/"><a>Home</a></Link>
 			<Link href="/about"><a>About</a></Link>
--- Cursos/The Net Ninja/Next.js/ninjalist/pages/about.js
+++ Cursos/The Net Ninja/Next.js/ninjalist/pages/about.js
@@ -1,10 +1,18 @@
+import Head from "next/head";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo atque aliquid quaerat nesciunt totam dignissimos nihil ipsa est eveniet. Beatae nemo totam adipisci voluptatem. Libero minima quisquam eos vero? Quod.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo atque aliquid quaerat nesciunt totam dignissimos nihil ipsa est eveniet. Beatae nemo totam adipisci voluptatem. Libero minima quisquam eos vero? Quod.</p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo atque aliquid quaerat nesciunt totam dignissimos nihil ipsa est eveniet. Beatae nemo totam adipisci voluptatem. Libero minima quisquam eos vero? Quod.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo atque aliquid quaerat nesciunt totam dignissimos nihil ipsa est eveniet. Beatae nemo totam adipisci voluptatem. Libero minima quisquam eos vero? Quod.</p>
+      </div>
+    </>
   )
 }
 
