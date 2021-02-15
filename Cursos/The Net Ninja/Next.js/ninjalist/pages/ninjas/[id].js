a26e80f617ee27b7a822b9943dc211c0133a2af9
Luís Galete <luisgalete6264@gmail.com>
2021-02-15 13:22:55 -0300

    getStaticPaths

--- Cursos/The Net Ninja/Next.js/ninjalist/pages/ninjas/[id].js
+++ Cursos/The Net Ninja/Next.js/ninjalist/pages/ninjas/[id].js
@@ -1,3 +1,17 @@
+export const getStaticPaths = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  const paths = data.map(ninja => {
+    return { params: { id: ninja.id.toString() } }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
 const Details = () => {
   return (
     <div>
