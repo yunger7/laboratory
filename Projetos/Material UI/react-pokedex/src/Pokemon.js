8b4214d3654f50b2dd152728c52bfdd3275454d4
Luís Galete <luisgalete6264@gmail.com>
2021-02-16 10:10:48 -0300

    Grid Layout + Cards

--- Projetos/Material UI/react-pokedex/src/Pokemon.js
+++ Projetos/Material UI/react-pokedex/src/Pokemon.js
@@ -1,8 +1,36 @@
+import { AppBar, Toolbar, Grid, Card, CardMedia, CardContent } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  pokedexContainer: {
+    padding: '20px 50px',
+  }
+})
+
+const getPokemonCard = () => {
+  return (
+    <Grid item xs={12} sm={4}>
+      <Card>
+        <CardContent>
+          Hello
+        </CardContent>
+      </Card>
+    </Grid>
+  )
+}
+
 const Pokemon = (props) => {
-  const pokemonId = props.match.params.pokemonId;
+  const classes = useStyles();
 
   return (
-    <div>Pokemon #{ pokemonId }</div>
+    <>
+      <AppBar position="static">
+        <Toolbar></Toolbar>
+      </AppBar>
+      <Grid container spacing={2} className={classes.pokedexContainer}>
+        {getPokemonCard()}
+      </Grid>
+    </>
   );
 }
  
