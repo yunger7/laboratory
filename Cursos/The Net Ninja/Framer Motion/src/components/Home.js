c4249c0ca263f4994d68de8fcdb1f73d39ed0fd8
Luís Galete <luisgalete6264@gmail.com>
2021-02-17 17:09:17 -0300

    Animating Elements

--- Cursos/The Net Ninja/Framer Motion/src/components/Home.js
+++ Cursos/The Net Ninja/Framer Motion/src/components/Home.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
-  return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
-      <Link to="/base">
-        <button>
-          Create Your Pizza
-        </button>
-      </Link>
-    </div>
-  )
-}
+	return (
+		<motion.div className="home container"
+      animate={{}}
+    >
+			<h2>Welcome to Pizza Joint</h2>
+			<Link to="/base">
+				<motion.button
+          animate={{}}
+        >Create Your Pizza</motion.button>
+			</Link>
+		</motion.div>
+	);
+};
 
 export default Home;
