ffd6fe49d0bd068ffb0b4db7dce0b2338e4aaea7
Luís Galete <luisgalete6264@gmail.com>
2021-02-18 11:02:32 -0300

    Variants (part 1)

--- Cursos/The Net Ninja/Framer Motion/src/components/Base.js
+++ Cursos/The Net Ninja/Framer Motion/src/components/Base.js
@@ -2,14 +2,42 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from "framer-motion";
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw'
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      delay: 0.5
+    }
+  },
+};
+
+const nextVariants = {
+  hidden: {
+    x: '-100vw'
+  },
+  visible: {
+    x: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 120
+    }
+  }
+};
+
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
   return (
     <motion.div className="base container"
-      initial={{ x: '100vw' }}
-      animate={{ x: 0 }}
-      transition={{ type: 'spring', delay: 0.5 }}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
     >
 
       <h3>Step 1: Choose Your Base</h3>
@@ -28,11 +56,8 @@ const Base = ({ addBase, pizza }) => {
       </ul>
 
       {pizza.base && (
-        <motion.div
-          className="next"
-          initial={{ x: '-100vw' }}
-          animate={{ x: 0 }}
-          transition={{ type: 'spring', stiffness: 120 }}
+        <motion.div className="next"
+          variants={nextVariants}
         >
           <Link to="/toppings">
             <motion.button
