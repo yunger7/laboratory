f00c1438a503e367f327d9df39fdb90c7a4ca463
Luís Galete <luisgalete6264@gmail.com>
2021-02-18 11:54:23 -0300

    Variants (part 2)

--- Cursos/The Net Ninja/Framer Motion/src/components/Order.js
+++ Cursos/The Net Ninja/Framer Motion/src/components/Order.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React from "react";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw'
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      mass: 0.4,
+      damping: 8,
+      when: "beforeChildren",
+      staggerChildren: 0.75
+    }
+  },
+};
+
+const childrenVariants = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1
+  }
+}
 
 const Order = ({ pizza }) => {
-  return (
-    <div className="container order">
-      <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
-    </div>
-  )
-}
+	return (
+		<motion.div className="container order"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
+			<h2>Thank you for your order :)</h2>
+			<motion.p variants={childrenVariants}>You ordered a {pizza.base} pizza with:</motion.p>
+      <motion.div variants={childrenVariants}>
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
+      </motion.div>
+		</motion.div>
+	);
+};
 
 export default Order;
