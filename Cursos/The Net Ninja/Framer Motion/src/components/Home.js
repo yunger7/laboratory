e8c1cfa4b9cc780ff630cd70fb6f556302c7ea12
Luís Galete <luisgalete6264@gmail.com>
2021-02-21 12:01:24 -0300

    Lodaer

--- Cursos/The Net Ninja/Framer Motion/src/components/Home.js
+++ Cursos/The Net Ninja/Framer Motion/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import Loader from "./Loader";
 
 const buttonVariants = {
 	hover: {
@@ -48,6 +49,7 @@ const Home = () => {
 					Create Your Pizza
 				</motion.button>
 			</Link>
+			<Loader />
 		</motion.div>
 	);
 };
