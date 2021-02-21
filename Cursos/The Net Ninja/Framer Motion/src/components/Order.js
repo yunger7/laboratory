a2fe5d640771eff4c53e64a8b31f69421fdb5770
Luís Galete <luisgalete6264@gmail.com>
2021-02-21 10:54:08 -0300

    Modal Backdrop

--- Cursos/The Net Ninja/Framer Motion/src/components/Order.js
+++ Cursos/The Net Ninja/Framer Motion/src/components/Order.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
 const containerVariants = {
@@ -34,7 +34,14 @@ const childrenVariants = {
   }
 };
 
-const Order = ({ pizza }) => {
+const Order = ({ pizza, setShowModal }) => {
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowModal(true);
+    }, 5000)
+  }, [setShowModal]);
+
 	return (
 		<motion.div className="container order"
       variants={containerVariants}
