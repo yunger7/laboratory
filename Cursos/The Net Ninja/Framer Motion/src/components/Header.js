9eb5f4dc8c9f561803922cac2f8e838185dc0921
Luís Galete <luisgalete6264@gmail.com>
2021-02-21 11:47:15 -0300

    SVG Animation

--- Cursos/The Net Ninja/Framer Motion/src/components/Header.js
+++ Cursos/The Net Ninja/Framer Motion/src/components/Header.js
@@ -1,31 +1,61 @@
-import React from 'react';
+import React from "react";
 import { motion } from "framer-motion";
 
+const svgVariants = {
+	initial: {
+		rotate: -180,
+	},
+	animate: {
+		rotate: 0,
+		transition: { duration: 1 },
+	},
+};
+
+const pathVariants = {
+  initial: {
+    opacity: 0,
+    pathLength: 0,
+  },
+  animate: {
+    opacity: 1,
+    pathLength: 1,
+    transition: {
+      duration: 2,
+      ease: "easeInOut"
+    },
+  },
+};
+
 const Header = () => {
-  return (
-    <header>
-      <div className="logo">
-        <svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-          <path
-            fill="none"
-            d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
-          />
-          <path
-            fill="none"
-            d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
-          />
-        </svg>
-      </div>
-      <motion.div
-        className="title"
-        initial={{ y: -250 }}
-        animate={{ y: -10 }}
-        transition={{ delay: 0.2, type: 'spring' }}
-      >
-        <h1>Pizza Joint</h1>
-      </motion.div>
-    </header>
-  )
-}
+	return (
+		<header>
+			<div className="logo">
+				<motion.svg
+					className="pizza-svg"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 100 100"
+					variants={svgVariants}
+          initial="initial"
+          animate="animate"
+				>
+					<motion.path
+						fill="none"
+						d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+            variants={pathVariants}
+					/>
+					<motion.path fill="none" d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z" variants={pathVariants} />
+				</motion.svg>
+			</div>
+			<motion.div
+				className="title"
+				initial={{ y: -250 }}
+				animate={{ y: -10 }}
+				transition={{ delay: 0.2, type: "spring" }}
+			>
+				<h1>Pizza Joint</h1>
+			</motion.div>
+		</header>
+	);
+};
 
 export default Header;
