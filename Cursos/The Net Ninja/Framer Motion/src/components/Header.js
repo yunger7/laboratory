cc2d77031343d261a229bfc1cbedbde3d325ca20
Luís Galete <luisgalete6264@gmail.com>
2021-02-21 14:24:34 -0300

    Draggin items

--- Cursos/The Net Ninja/Framer Motion/src/components/Header.js
+++ Cursos/The Net Ninja/Framer Motion/src/components/Header.js
@@ -12,40 +12,49 @@ const svgVariants = {
 };
 
 const pathVariants = {
-  initial: {
-    opacity: 0,
-    pathLength: 0,
-  },
-  animate: {
-    opacity: 1,
-    pathLength: 1,
-    transition: {
-      duration: 2,
-      ease: "easeInOut"
-    },
-  },
+	initial: {
+		opacity: 0,
+		pathLength: 0,
+	},
+	animate: {
+		opacity: 1,
+		pathLength: 1,
+		transition: {
+			duration: 2,
+			ease: "easeInOut",
+		},
+	},
 };
 
 const Header = () => {
 	return (
 		<header>
-			<div className="logo">
+			<motion.div
+				className="logo"
+				drag
+				dragConstraints={{ left: 0, top: 0, right: 0, bottom: 0 }}
+				dragElastic={0.2}
+			>
 				<motion.svg
 					className="pizza-svg"
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 100 100"
 					variants={svgVariants}
-          initial="initial"
-          animate="animate"
+					initial="initial"
+					animate="animate"
 				>
 					<motion.path
 						fill="none"
 						d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
-            variants={pathVariants}
+						variants={pathVariants}
 					/>
-					<motion.path fill="none" d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z" variants={pathVariants} />
+					<motion.path
+						fill="none"
+						d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+						variants={pathVariants}
+					/>
 				</motion.svg>
-			</div>
+			</motion.div>
 			<motion.div
 				className="title"
 				initial={{ y: -250 }}
