e503c99a4a9eba2b5bd87b4ea779fc423c7b471f
Luís Galete <luisgalete6264@gmail.com>
2021-02-24 17:55:40 -0300

    Fetch coin data

--- Projetos/Next/bitnext/components/Navbar.js
+++ Projetos/Next/bitnext/components/Navbar.js
@@ -1,26 +1,37 @@
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import Link from "next/link";
 import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
 import { MonetizationOn } from "@material-ui/icons";
 
 const useStyles = makeStyles({
-  logo: {
-    marginRight: 6,
-  }
+	logo: {
+		marginRight: 6,
+		display: "flex",
+		flexGrow: 1,
+	},
+	logoIcon: {
+		marginRight: 18,
+	},
 });
 
 const Navbar = () => {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <MonetizationOn fontSize="large" className={classes.logo} />
-        <Typography component="h1" variant="h6">
-          BitNext
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  );
-}
- 
+	return (
+		<AppBar position="static" className={classes.root}>
+			<Toolbar>
+				<div className={classes.logo}>
+					<MonetizationOn fontSize="large" className={classes.logoIcon} />
+					<Typography component="h1" variant="h6">
+						BitNext
+					</Typography>
+				</div>
+        <Link href="/about">
+				  <Button color="inherit">About</Button>
+        </Link>
+			</Toolbar>
+		</AppBar>
+	);
+};
+
 export default Navbar;
--- Projetos/Next/bitnext/pages/index.js
+++ Projetos/Next/bitnext/pages/index.js
@@ -1,33 +1,60 @@
 import Head from "next/head";
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography } from "@material-ui/core";
+import { Typography, Grid } from "@material-ui/core";
 import CryptoCard from "../components/CryptoCard";
 
 const useStyles = makeStyles({
-  title: {
-    textAlign: "center",
-    marginTop: 20,
-  },
-  subTitle: {
-    textAlign: "center",
-  }
+	title: {
+		textAlign: "center",
+		marginTop: 20,
+	},
+	subTitle: {
+		textAlign: "center",
+	},
+	main: {
+		marginTop: 24,
+	},
 });
 
-export default function Home() {
-  const classes = useStyles();
+export default function Home({ coins }) {
+	const classes = useStyles();
+  console.log(coins);
 
 	return (
 		<div>
 			<Head>
 				<title>BitNext</title>
 			</Head>
-      <div className="title">
-        <Typography variant="h4" className={classes.title}>Welcome do BitNext!</Typography>
-        <Typography className={classes.subTitle}>Check your crypto prices</Typography>
-      </div>
-			<main>
-        <CryptoCard />
-			</main>
+			<div className="title">
+				<Typography variant="h4" className={classes.title}>
+					Welcome do BitNext!
+				</Typography>
+				<Typography className={classes.subTitle}>
+					Check your crypto prices
+				</Typography>
+			</div>
+			<Grid container spacing={3} component="main" className={classes.main}>
+        {coins.forEach(coin => (
+          <Grid item xs={4}>
+            <CryptoCard coin={coin} />
+          </Grid>
+        ))}
+			</Grid>
 		</div>
 	);
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://api.coinlore.net/api/tickers/');
+  const json = await res.json();
+
+  // filter results
+  const wantedCoins = [90, 80, 257, 1, 2, 33830];
+  const data = json.data.filter(coin => wantedCoins.includes(parseInt(coin.id)));
+
+  return {
+    props: {
+      coins: data
+    }
+  }
 }
