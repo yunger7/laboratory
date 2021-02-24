9252f70fe3fc29593644851edc30bbb37450f1a3
Luís Galete <luisgalete6264@gmail.com>
2021-02-24 20:18:23 -0300

    Change homepage to display total cryptos

--- Projetos/Next/bitnext/pages/index.js
+++ Projetos/Next/bitnext/pages/index.js
@@ -16,7 +16,7 @@ const useStyles = makeStyles({
 	},
 });
 
-export default function Home({ coins }) {
+export default function Home({ coins, totalCoins }) {
 	const classes = useStyles();
 
 	return (
@@ -29,31 +29,38 @@ export default function Home({ coins }) {
 					Welcome do BitNext!
 				</Typography>
 				<Typography variant="subtitle1" className={classes.subTitle}>
-					Check your crypto prices
+					Here are 6 out of the {totalCoins} cryptos in existence.
 				</Typography>
 			</div>
 			<Grid container spacing={3} component="main" className={classes.main}>
-        {coins.map(coin => (
-          <Grid item xs={12} sm={6} md={4} key={coin.id}>
-            <CryptoCard coin={coin} />
-          </Grid>
-        ))}
+				{coins.map(coin => (
+					<Grid item xs={12} sm={6} md={4} key={coin.id}>
+						<CryptoCard coin={coin} />
+					</Grid>
+				))}
 			</Grid>
 		</div>
 	);
-};
+}
 
 export const getStaticProps = async () => {
-  const res = await fetch('https://api.coinlore.net/api/tickers/');
-  const json = await res.json();
+	const coinsRes = await fetch("https://api.coinlore.net/api/tickers/");
+	const coinsJson = await coinsRes.json();
 
-  // filter results
-  const wantedCoins = [90, 80, 257, 1, 2, 33830];
-  const data = json.data.filter(coin => wantedCoins.includes(parseInt(coin.id)));
+	// filter results
+	const wantedCoins = [90, 80, 257, 1, 2, 33830];
+	const coinsData = coinsJson.data.filter(coin =>
+		wantedCoins.includes(parseInt(coin.id))
+	);
 
-  return {
-    props: {
-      coins: data
-    }
-  }
-}
+	const globalRes = await fetch("https://api.coinlore.net/api/global/");
+	const globalJson = await globalRes.json();
+	const totalCoins = globalJson[0].coins_count;
+
+	return {
+		props: {
+			coins: coinsData,
+			totalCoins,
+		},
+	};
+};
