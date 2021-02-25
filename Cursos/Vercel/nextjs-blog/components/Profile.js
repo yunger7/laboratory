7ed8c18a6b6211f8fa10a95a727b3019a49f18c7
Luís Galete <luisgalete6264@gmail.com>
2021-02-25 17:21:04 -0300

    Links, Assets, Metadata and CSS

---
+++ Cursos/Vercel/nextjs-blog/components/Profile.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+const Profile = () => {
+  return (
+    <Image src="/images/profile.jpg" alt="Profile picture" width={144} height={144} />
+  );
+}
+ 
+export default Profile;
