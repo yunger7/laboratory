af10ea00f8812bebf6c3e7b4111ed2be5bbb4e59
Luís Galete <luisgalete6264@gmail.com>
2021-03-09 18:22:24 -0300

    Connect to MongoDB

---
+++ Courses/MongoDB/The Net Ninja/test/connection.js
@@ -0,0 +1,10 @@
+import Mongoose from "mongoose";
+
+Mongoose.connect("mongodb://localhost/testaroo");
+Mongoose.connection
+	.once("open", () => {
+		console.log("> Connected to MongoDB");
+	})
+	.on("error", error => {
+		console.log("> Connection error:", error);
+	});
