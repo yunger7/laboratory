90fa36023fc497140629e5f8338ca7f17e6c8c50
Luís Galete <luisgalete6264@gmail.com>
2021-03-12 14:13:45 -0300

    ES6 Promises

--- Courses/MongoDB/The Net Ninja/test/connection.js
+++ Courses/MongoDB/The Net Ninja/test/connection.js
@@ -1,10 +1,18 @@
 const Mongoose = require("mongoose");
 
-Mongoose.connect("mongodb://localhost/testaroo");
-Mongoose.connection
-	.once("open", () => {
-		console.log("> Connected to MongoDB");
-	})
-	.on("error", error => {
-		console.log("> Connection error:", error);
-	});
+// ES6 Promises
+Mongoose.Promise = global.Promise;
+
+// Connect to DB before tests
+before((done) => {
+	// Connect to MongoDB
+	Mongoose.connect("mongodb://localhost/testaroo");
+	Mongoose.connection
+		.once("open", () => {
+			console.log("> Connected to MongoDB");
+			done();
+		})
+		.on("error", error => {
+			console.log("> Connection error:", error);
+		});
+});
