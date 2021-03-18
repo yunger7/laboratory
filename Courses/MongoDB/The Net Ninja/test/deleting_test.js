a07c7db40464adb76487f9ff4fab8535b226317b
Luís Galete <luisgalete6264@gmail.com>
2021-03-18 18:09:45 -0300

    Deleting Records

---
+++ Courses/MongoDB/The Net Ninja/test/deleting_test.js
@@ -0,0 +1,26 @@
+const assert = require("assert");
+const MarioChar = require("../models/mariochar");
+
+describe("Deleting records", function() {
+  var char;
+
+  beforeEach(function(done){
+    char = new MarioChar({
+      name: 'Mario',
+    });
+  
+    char.save().then(() => {
+      done();
+    });
+  })
+
+  it('Deletes one record from the database', function(done) {
+    MarioChar.findOneAndRemove({ name: "Mario" }).then(function(){
+      MarioChar.findOne({ name: "Mario" }).then(function(result){
+        assert(result === null);
+        done();
+      });
+    });
+  });
+
+});
