60a2eec614c955c4a0130866f3177a10703585b5
Luís Galete <luisgalete6264@gmail.com>
2021-03-20 18:33:23 -0300

    Icons

--- Courses/Material-UI/The Net Ninja/src/pages/Create.js
+++ Courses/Material-UI/The Net Ninja/src/pages/Create.js
@@ -2,22 +2,29 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 export default function Create() {
 	return (
 		<Container>
 			<Typography
-        variant="h6"
-        color="textSecondary"
-        component="h2"
-        gutterBottom
-      >
-        Create a New Note
-      </Typography>
+				variant="h6"
+				color="textSecondary"
+				component="h2"
+				gutterBottom
+			>
+				Create a New Note
+			</Typography>
 
-      <Button type="submit" variant="contained" color="secondary" onClick={() => console.log('You clicked me!')}>
-        Submit
-      </Button>
+			<Button
+				type="submit"
+				variant="contained"
+				color="secondary"
+        endIcon={<KeyboardArrowRightIcon />}
+				onClick={() => console.log("You clicked me!")}
+			>
+				Submit
+			</Button>
 		</Container>
 	);
 }
