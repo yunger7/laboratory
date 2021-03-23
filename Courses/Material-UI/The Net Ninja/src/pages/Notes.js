0f4d58fb3192f6ecfb2d83e35db81c2f342dbf38
Luís Galete <luisgalete6264@gmail.com>
2021-03-23 17:07:02 -0300

    Grid system

--- Courses/Material-UI/The Net Ninja/src/pages/Notes.js
+++ Courses/Material-UI/The Net Ninja/src/pages/Notes.js
@@ -1,19 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 
 export default function Notes() {
-  const [notes, setNotes] = useState([]);
+	const [notes, setNotes] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:8000/notes")
-      .then(res => res.json())
-      .then(data => setNotes(data));
-  }, [])
+	useEffect(() => {
+		fetch("http://localhost:8000/notes")
+			.then(res => res.json())
+			.then(data => setNotes(data));
+	}, []);
 
-  return (
-    <div>
-      {notes.map(note => (
-        <p key={note.id}>{ note.title }</p>
-      ))}
-    </div>
-  )
+	return (
+		<div>
+      {/* <Grid container>
+        <Grid item md={3} sm={6} xs={12}>
+          <Paper>1</Paper>
+        </Grid>
+        <Grid item md={3} sm={6} xs={12}>
+          <Paper>2</Paper>
+        </Grid>
+        <Grid item md={3} sm={6} xs={12}>
+          <Paper>3</Paper>
+        </Grid>
+        <Grid item md={3} sm={6} xs={12}>
+          <Paper>3</Paper>
+        </Grid>
+      </Grid> */}
+
+      <Grid container>
+        {notes.map(note => (
+          <Grid item key={note.id} xs={12} md={6} lg={4}>
+            <Paper>{note.title}</Paper>
+          </Grid>
+        ))}
+      </Grid>
+		</div>
+	);
 }
