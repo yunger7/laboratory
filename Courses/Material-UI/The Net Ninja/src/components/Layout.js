5db017d45db0c6b6b78b74d57131a754b4259f63
Luís Galete <61747990+yunger7@users.noreply.github.com>
2021-03-26 12:11:57 -0300

    Create Drawer

--- Courses/Material-UI/The Net Ninja/src/components/Layout.js
+++ Courses/Material-UI/The Net Ninja/src/components/Layout.js
@@ -1,9 +1,21 @@
 import { makeStyles } from "@material-ui/core/styles";
+import { Drawer, Typography } from "@material-ui/core";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles({
+  root: {
+    display: "flex",
+  },
   page: {
     background: "#f9f9f9",
     width: "100%",
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
   }
 });
 
@@ -11,7 +23,23 @@ export default function Layout({ children }) {
   const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.root}>
+
+      {/* App bar */}
+
+      {/* Side drawer */}
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography variant="h5">
+            Ninja Notes
+          </Typography>
+        </div>
+      </Drawer>
 
       <div className={classes.page}>
         { children }
