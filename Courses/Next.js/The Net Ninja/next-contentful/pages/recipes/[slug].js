5587d7fe2f83e95c3a7ac88d89071dbaa72db63c
Luís Galete <61747990+yunger7@users.noreply.github.com>
2021-04-25 18:37:55 -0300

    Generating Paths

--- Courses/Next.js/The Net Ninja/next-contentful/pages/recipes/[slug].js
+++ Courses/Next.js/The Net Ninja/next-contentful/pages/recipes/[slug].js
@@ -1,7 +1,40 @@
-export default function RecipeDetails() {
-  return (
-    <div>
-      Recipe Details
-    </div>
-  )
+import { createClient } from "contentful";
+
+const client = createClient({
+	space: process.env.CONTENTFUL_SPACE_ID,
+	accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export async function getStaticPaths() {
+	const res = await client.getEntries({ content_type: "recipe" });
+
+	const paths = res.items.map(item => {
+		return { params: { slug: item.fields.slug } };
+	});
+
+	return {
+		paths,
+    fallback: false,
+	};
+}
+
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+
+  const { items } = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": slug,
+  });
+
+  return {
+    props: {
+      recipe: items[0],
+    },
+  };
+}
+
+export default function RecipeDetails({ recipe }) {
+  console.log(recipe);
+
+	return <div>Recipe Details</div>;
 }
