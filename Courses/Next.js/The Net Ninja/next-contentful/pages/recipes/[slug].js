e967be92ffcf1c0a800176f626f995fdaef30bae
Luís Galete <61747990+yunger7@users.noreply.github.com>
2021-04-25 18:58:14 -0300

    Rich Text Content

--- Courses/Next.js/The Net Ninja/next-contentful/pages/recipes/[slug].js
+++ Courses/Next.js/The Net Ninja/next-contentful/pages/recipes/[slug].js
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import { createClient } from "contentful";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 const client = createClient({
 	space: process.env.CONTENTFUL_SPACE_ID,
@@ -14,27 +16,85 @@ export async function getStaticPaths() {
 
 	return {
 		paths,
-    fallback: false,
+		fallback: false,
 	};
 }
 
 export async function getStaticProps({ params }) {
-  const { slug } = params;
+	const { slug } = params;
 
-  const { items } = await client.getEntries({
-    content_type: "recipe",
-    "fields.slug": slug,
-  });
+	const { items } = await client.getEntries({
+		content_type: "recipe",
+		"fields.slug": slug,
+	});
 
-  return {
-    props: {
-      recipe: items[0],
-    },
-  };
+	return {
+		props: {
+			recipe: items[0],
+		},
+	};
 }
 
 export default function RecipeDetails({ recipe }) {
-  console.log(recipe);
+	console.log(recipe);
+	const {
+		featuredImage,
+		title,
+		cookingTime,
+		ingredients,
+		method,
+	} = recipe.fields;
 
-	return <div>Recipe Details</div>;
+	return (
+		<div>
+			<div className="banner">
+				<Image
+					src={`https:${featuredImage.fields.file.url}`}
+					width={featuredImage.fields.file.details.image.width}
+					height={featuredImage.fields.file.details.image.height}
+				/>
+				<h2>{title}</h2>
+			</div>
+
+			<div className="info">
+				<p>Takes about {cookingTime} mins to cook.</p>
+				<h3>Ingredients</h3>
+
+				{ingredients.map(ing => (
+					<span key={ing}>{ing}</span>
+				))}
+			</div>
+
+      <div className="method">
+        <h3>Method:</h3>
+        <div>{documentToReactComponents(method)}</div>
+      </div>
+
+      <style jsx>{`
+        h2,h3 {
+          text-transform: uppercase;
+        }
+        .banner h2 {
+          margin: 0;
+          background: #fff;
+          display: inline-block;
+          padding: 20px;
+          position: relative;
+          top: -60px;
+          left: -10px;
+          transform: rotateZ(-1deg);
+          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+        }
+        .info p {
+          margin: 0;
+        }
+        .info span::after {
+          content: ", ";
+        }
+        .info span:last-child::after {
+          content: ".";
+        }
+      `}</style>
+		</div>
+	);
 }
