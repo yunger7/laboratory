1b7feb867951073641a3d9783a3745e248a53ff2
Luís Galete <61747990+yunger7@users.noreply.github.com>
2021-04-28 12:56:54 -0300

    Function basics

--- Courses/Typescript/The Net Ninja/public/sandbox.js
+++ Courses/Typescript/The Net Ninja/public/sandbox.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log("hello");
+var greet;
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(10, 5);
--- Courses/Typescript/The Net Ninja/src/sandbox.ts
+++ Courses/Typescript/The Net Ninja/src/sandbox.ts
@@ -1,1 +1,8 @@
-console.log("hello");
+let greet: Function;
+
+const add = (a: number, b: number, c: number | string = 10) => {
+  console.log(a + b);
+  console.log(c);
+}
+
+add(10, 5);
